bdd5e630fce3c1bb60f0adf1d3ea17e9de1a4ddf
Mario Loriedo <mario.loriedo@gmail.com>
2026-07-15 16:57:59 +0200

    Add host ip retrieval
    Signed-off-by: Mario Loriedo <mario.loriedo@gmail.com>

---
+++ go.mod
@@ -0,0 +1,10 @@
+module lorie.do/wsl-test
+
+go 1.26.4
+
+require github.com/vishvananda/netlink v1.3.1
+
+require (
+	github.com/vishvananda/netns v0.0.5 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/vishvananda/netlink"
+)
+
+const defaultWSLRoute = "0.0.0.0/0"
+
+// wslHostIP returns the Windows host's IP address. It only make
+// sense to execute it when running in a WSL distribution.
+// Instructions to retrieve the IP address are section "Identify IP address"
+// (scenario 2) of the WSL networking documentation:
+// https://learn.microsoft.com/en-us/windows/wsl/networking#identify-ip-address
+func wslHostIP() string {
+	routes, err := netlink.RouteList(nil, netlink.FAMILY_V4)
+	if err != nil {
+		return ""
+	}
+	for _, r := range routes {
+		if r.Dst.String() == defaultWSLRoute && r.Gw != nil {
+			return r.Gw.String()
+		}
+	}
+	return ""
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	hip := wslHostIP()
+	fmt.Println(hip)
 }
